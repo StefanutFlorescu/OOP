dbe04565fddc6797e9cf99f1f9c2386899e995e3
Florescu <florescu.stefanut@gmail.com>
2024-11-02 15:57:04 +0200

    Testing_Configured

--- generated/include/test.h
+++ generated/include/test.h
@@ -4,7 +4,8 @@
 
 #ifndef TEST_H
 #define TEST_H
-
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "doctest.h"
 
 
 #endif //TEST_H
--- generated/src/test.cpp
+++ generated/src/test.cpp
@@ -1,5 +1,9 @@
 //
 // Created by stef on 10/27/24.
 //
-
 #include "../include/test.h"
+
+TEST_CASE("Test functional") {
+    CHECK(1 + 1 == 2);
+    CHECK(2 * 2 == 4);
+}
